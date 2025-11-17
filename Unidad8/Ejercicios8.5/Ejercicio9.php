f8c619d4448563dd82b6b4b0b3044f2ec463c7c9
JonathanIbanez <jonathan.ibanez-pinero@iesruizgijon.com>
2025-11-17 14:21:32 +0100

    Ejercicio9

--- Unidad8/Ejercicios8.5/Ejercicio9.php
+++ Unidad8/Ejercicios8.5/Ejercicio9.php
@@ -9,8 +9,26 @@ cuenta que los ficheros de donde se van cogiendo las líneas pueden tener tamañ
   */
 
 if (isset($_POST['add'])) {
-    $archivo1 = $_POST['archivo1'];
-    $archivo2 = $_POST['archivo2'];
+  $archivo1 = $_POST['archivo1'];
+  $archivo2 = $_POST['archivo2'];
+  $fp1 = fopen($archivo1, "r");
+  $fp2 = fopen($archivo2, "r");
+  $fpNuevo = fopen("combinacion.txt", "a");
+  while (!feof($fp1) && !feof($fp2)) {
+
+    if (!feof($fp1)) {
+      $linea1 = fgets($fp1);
+      fwrite($fpNuevo, $linea1 . PHP_EOL);
+    }
+
+    if (!feof($fp2)) {
+      $linea2 = fgets($fp2);
+      fwrite($fpNuevo, $linea2 . PHP_EOL);
+    }
+  }
+  fclose($fp1);
+  fclose($fp2);
+  fclose($fpNuevo);
 }
 ?>
 
@@ -18,21 +36,21 @@ if (isset($_POST['add'])) {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio9</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ejercicio9</title>
 </head>
 
 <body>
-    <form enctype="multipart/form-data" action="" method="POST">
-        <label>Recoge el primer archivo</label>
-        <input type="file" name="archivo1" required>
-        <br><br>
-        <label>Recoge el segundo archivo</label>
-        <input type="file" name="archivo2" required>
-        <br><br>
-        <input type="submit" name="add" value="Añadir">
-    </form>
+  <form enctype="multipart/form-data" action="" method="POST">
+    <label>Recoge el primer archivo</label>
+    <input type="file" name="archivo1" required>
+    <br><br>
+    <label>Recoge el segundo archivo</label>
+    <input type="file" name="archivo2" required>
+    <br><br>
+    <input type="submit" name="add" value="Añadir">
+  </form>
 </body>
 
 </html>
